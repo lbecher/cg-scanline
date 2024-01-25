deb798543417cfff3e52b5eb5a8801cfb33042f5
Luiz Fernando Becher de Araujo <luiz.becher.araujo@gmail.com>
2024-01-25 17:18:23 -0300

    desenho de arrestas

--- src/triangles.rs
+++ src/triangles.rs
@@ -1,5 +1,6 @@
 use bevy::{
     prelude::*,
+    render::render_resource::*,
     window::PrimaryWindow,
 };
 use bevy_egui::EguiContexts;
@@ -47,6 +48,9 @@ pub struct Triangle {
     pub redraw: bool,
 }
 
+#[derive(Component)]
+pub struct TriangleSprite(Option<Entity>);
+
 
 fn creating(
     mut commands: Commands,
@@ -93,12 +97,15 @@ fn creating(
             let mut sorted_triangle = state.new_triangle.clone();
             sorted_triangle.sort_by(|a, b| a.position[0].partial_cmp(&b.position[0]).unwrap_or(Ordering::Equal));
 
-            let entity = commands.spawn(Triangle {
-                first: sorted_triangle[0].clone(),
-                middle: sorted_triangle[1].clone(),
-                last: sorted_triangle[2].clone(),
-                redraw: true,
-            }).id();
+            let entity = commands.spawn((
+                Triangle {
+                    first: sorted_triangle[0].clone(),
+                    middle: sorted_triangle[1].clone(),
+                    last: sorted_triangle[2].clone(),
+                    redraw: true,
+                },
+                TriangleSprite(None),
+            )).id();
 
             state.function = Function::Modify(entity);
             state.new_triangle.clear();
@@ -131,12 +138,15 @@ fn modifying(
                         match vertex_selector.0 {
                             VertexOrder::First => {
                                 triangle.first.color = state.color_picker;
+                                triangle.redraw = true;
                             }
                             VertexOrder::Middle => {
                                 triangle.middle.color = state.color_picker;
+                                triangle.redraw = true;
                             }
                             VertexOrder::Last => {
                                 triangle.last.color = state.color_picker;
+                                triangle.redraw = true;
                             }
                             VertexOrder::Indifferent => {}
                         }
@@ -219,16 +229,56 @@ fn modifying(
 }
 
 
+
 fn redrawing(
-    mut egui_contexts: EguiContexts,
-    input: Res<Input<MouseButton>>,
-    mut state: ResMut<State>,
-    mut triangles_query: Query<&mut Triangle>,
+    mut commands: Commands,
+    mut images: ResMut<Assets<Image>>,
+    mut triangles_query: Query<(&mut Triangle, &mut TriangleSprite)>,
     window_query: Query<&Window, With<PrimaryWindow>>,
 ) {
-    for triangle in triangles_query.iter_mut() {
+    for (mut triangle, mut sprite) in triangles_query.iter_mut() {
         if triangle.redraw {
+            let window = window_query.single();
 
+            if let Some(entity) = sprite.0 {
+                commands.entity(entity).despawn();
+            }
+
+            let mut image = Image::new_fill(
+                Extent3d {
+                    width: window.width() as u32,
+                    height: window.height() as u32,
+                    depth_or_array_layers: 1,
+                },
+                TextureDimension::D2,
+                &[0, 0, 0, 64],
+                TextureFormat::Rgba8Unorm,
+            );
+            image.texture_descriptor.usage =
+                TextureUsages::COPY_DST | 
+                TextureUsages::STORAGE_BINDING | 
+                TextureUsages::TEXTURE_BINDING;
+            
+            render(&triangle, &mut image.data, window.width() as usize, window.height() as usize);
+
+            let image = images.add(image);
+
+            let entity = commands.spawn(SpriteBundle {
+                sprite: Sprite {
+                    custom_size: Some(Vec2::new(window.width(), window.height())),
+                    ..default()
+                },
+                texture: image.clone(),
+                transform: Transform::from_translation(Vec3::new(
+                    window.width() / 2.0,
+                    window.height() / 2.0,
+                    triangle.first.position[2],
+                )),
+                ..default()
+            }).id();
+
+            sprite.0 = Some(entity);
+            triangle.redraw = false;
         }
     }
 }
@@ -260,6 +310,99 @@ fn selecting(
 }
 
 
+fn render(
+    triangle: &Triangle,
+    image: &mut Vec<u8>,
+    width: usize,
+    height: usize,
+) {
+    for i in 0..3 {
+        let (
+            v0,
+            v1,
+        ) = if i == 0 {
+            (
+                triangle.first.clone(),
+                triangle.middle.clone(),
+            )
+        } else if i == 1 {
+            (
+                triangle.middle.clone(),
+                triangle.last.clone(),
+            )
+        } else {
+            (
+                triangle.last.clone(),
+                triangle.first.clone(),
+            )
+        };
+
+        let x0 = v0.position[0];
+        let y0 = v0.position[1];
+        let x1 = v1.position[0];
+        let y1 = v1.position[1];
+
+        let points = bresenham(x0, y0, x1, y1);
+        let points_len = points.len() as f32;
+
+        let mut count = 0.0;
+        for (x, y) in points {
+            let blend_factor = count / points_len;
+            count += 1.0;
+    
+            let r = v0.color[0] + blend_factor * (v1.color[0] - v0.color[0]);
+            let g = v0.color[1] + blend_factor * (v1.color[1] - v0.color[1]);
+            let b = v0.color[2] + blend_factor * (v1.color[2] - v0.color[2]);
+    
+            let i = height - (y.round() as usize);
+            let j = x.round() as usize;
+            let index = (i * width + j) * 4;
+            image[index] = (r * 255.0) as u8;
+            image[index + 1] = (g * 255.0) as u8;
+            image[index + 2] = (b * 255.0) as u8;
+            image[index + 3] = 255;
+        }
+    }
+
+    
+}
+
+
+fn bresenham(x0: f32, y0: f32, x1: f32, y1: f32) -> Vec<(f32, f32)> {
+    let mut result = Vec::new();
+    
+    let dx = (x1 - x0).abs();
+    let dy = (y1 - y0).abs();
+
+    let sx = if x0 < x1 { 1.0 } else { -1.0 };
+    let sy = if y0 < y1 { 1.0 } else { -1.0 };
+
+    let mut x = x0;
+    let mut y = y0;
+
+    let mut err = dx - dy;
+
+    while (x - x1).abs() > 0.1 || (y - y1).abs() > 0.1 {
+        result.push((x, y));
+
+        let e2 = 2.0 * err;
+
+        if e2 > -dy {
+            err -= dy;
+            x += sx;
+        }
+
+        if e2 < dx {
+            err += dx;
+            y += sy;
+        }
+    }
+
+    result.push((x1, y1));
+    result
+}
+
+
 fn barycentric_coordinates(p: (f32, f32), a: (f32, f32), b: (f32, f32), c: (f32, f32)) -> (f32, f32, f32) {
     let v0 = (c.0 - a.0, c.1 - a.1);
     let v1 = (b.0 - a.0, b.1 - a.1);
@@ -280,7 +423,7 @@ fn barycentric_coordinates(p: (f32, f32), a: (f32, f32), b: (f32, f32), c: (f32,
 
 
 fn is_inside(click: (f32, f32), triangle: &Triangle) -> bool {
-    let (u, v, w) = barycentric_coordinates(
+    let (u, v, _w) = barycentric_coordinates(
         click, 
         (triangle.first.position[0], triangle.first.position[1]), 
         (triangle.middle.position[0], triangle.middle.position[1]), 
